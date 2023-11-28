0bb2dee0b8cdbb21b643a5ef606b67323807146b
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-28 09:30:56 -0500

    Fin ajustes CFA

--- src/config/global.js
+++ src/config/global.js
@@ -150,7 +150,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/51220025_CF01_DU.pdf',
+        download: 'downloads/51220025_CF01_DU.zip',
       },
       {
         icono: 'fas fa-download',
